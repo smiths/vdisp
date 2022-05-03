9baafd4b75f48f2fc887b13dc0726f84242bb969
Emil Soleymani <emilsoleymani@Emils-MacBook-Pro.local>
2022-05-03 15:01:33 -0400

    added lerp function and testing to new branch

---
+++ src/vdisp.jl
@@ -0,0 +1,19 @@
+module vdisp
+export lerp
+
+function lerp(a::Tuple{Float64, Float64}, b::Tuple{Float64, Float64}, x::Float64)
+    x1 = a[1]
+    y1 = a[2]
+    x2 = b[1]
+    y2 = b[2]
+    if x1 == x2 && y1 == y2
+        println("Warning: both points are equal!")
+        return y1
+    end
+    numerator = y1 * (x2-x) + y2 * (x-x1)
+    denominator = x2 - x1
+    # println(numerator/denominator)
+    return numerator/denominator
+ end
+
+end # module
