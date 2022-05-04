bdd665bd964b2278d81fa66c4318548ada0cfd00
Documenter.jl <documenter@juliadocs.github.io>
2022-05-04 18:50:21 +0000

    build based on 23fe5b4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#vdisp.jl","page":"vdisp.jl","title":"vdisp.jl","text":"","category":"section"},{"location":"","page":"vdisp.jl","title":"vdisp.jl","text":"Documentation for vdisp.jl","category":"page"}]
+}
