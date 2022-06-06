5c1d08fd7944ed650395fc7780d07275edca717d
Documenter.jl <documenter@juliadocs.github.io>
2022-06-06 21:16:25 +0000

    build based on 4f3010a

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#vdisp.jl","page":"vdisp.jl","title":"vdisp.jl","text":"","category":"section"},{"location":"","page":"vdisp.jl","title":"vdisp.jl","text":"Documentation for vdisp.jl","category":"page"}]
+[{"location":"#vdisp.jl","page":"vdisp.jl","title":"vdisp.jl","text":"","category":"section"},{"location":"","page":"vdisp.jl","title":"vdisp.jl","text":"vdisp.readInputFile","category":"page"},{"location":"#vdisp.readInputFile","page":"vdisp.jl","title":"vdisp.readInputFile","text":"readInputFile(inputPath, outputPath)\n\nReads and parses input file at inputPath and outputs calculations to file at outputPath\n\n\n\n\n\n","category":"function"},{"location":"","page":"vdisp.jl","title":"vdisp.jl","text":"Documentation for vdisp.jl","category":"page"}]
 }
